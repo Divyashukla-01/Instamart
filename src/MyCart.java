9bebafc243361f0eb2dfa53a6b6c0037e986bf5c
Divyashukla-01 <divyashuklaa15@gmail.com>
2025-05-09 14:38:12 +0530

    first commit

---
+++ src/MyCart.java
@@ -0,0 +1,51 @@
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import BO.AddressBO;
+import BO.DiscountBO;
+import BO.ProductBO;
+import Domain.Address;
+import Domain.Discount;
+import Domain.Product;
+import Domain.User;
+
+/**
+ * Servlet implementation class MyCart
+ */
+@WebServlet("/MyCart")
+public class MyCart extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyCart() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//fill your code here
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ web/myCart.jsp
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html lang="en">
+<%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix = "c" %>
+
+
+</html>
